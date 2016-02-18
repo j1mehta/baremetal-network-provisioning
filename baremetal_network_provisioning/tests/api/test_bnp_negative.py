89e9b9412302aaa17618729516a23fa86e935263
Jatin <jatinmehta.jm@gmail.com>
2016-02-18 15:27:46 +0530

    Minor changes in License and sample file

--- baremetal_network_provisioning/tests/api/test_bnp_negative.py
+++ baremetal_network_provisioning/tests/api/test_bnp_negative.py
@@ -1,5 +1,5 @@
-# Copyright 2015 Hewlett-Packard Development Company, L.P.
-# Copyright 2015 OpenStack Foundation
+# Copyright 2016 Hewlett-Packard Development Company, L.P.
+# Copyright 2016 OpenStack Foundation
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
