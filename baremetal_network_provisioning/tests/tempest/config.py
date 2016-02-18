3d4736465615d4880d5fb654faba79a435161186
Jatin <jatinmehta.jm@gmail.com>
2016-02-18 14:33:49 +0530

    Negative test cases for BNP

---
+++ baremetal_network_provisioning/tests/tempest/config.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2016 Hewlett-Packard Enterprise Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from oslo_config import cfg
+
+# BMNP related config information
+
+hpeBNP_OPTS = [
+
+    cfg.StrOpt('ip_address',
+               default='',
+               help='Switch IP address'),
+    cfg.StrOpt('switch_user',
+               default='',
+               help='Switch user name to log in'),
+    cfg.StrOpt('switch_passwd',
+               default='',
+               help='Switch password to log in'),
+    cfg.StrOpt('vendor',
+               default='',
+               help='Specify vendor'),
+    cfg.DictOpt('access_parameter',
+                default='',
+                help='access_parameter for snmp v1 and v2'),
+    cfg.DictOpt('access_parameter_priv',
+                default='',
+                help='access_parameter_priv'),
+    cfg.DictOpt('access_parameter_v3',
+                default='',
+                help='access_parameter for snmp v3'),
+    cfg.StrOpt('access_protocol_v1',
+               default='',
+               help='access protocol snmpv1'),
+    cfg.StrOpt('access_protocol_v2c',
+               default='',
+               help='access protocol snmpv2'),
+    cfg.StrOpt('access_protocol_v3',
+               default='',
+               help='access protocol snmpv3'),
+
+]
+
+
+def register_options():
+    cfg.CONF.register_opts(hpeBNP_OPTS, "hpeBNP")
